6bce537803957ae70897eed683cb3ca4b5925ccf
Sivayogeith Umamaheswaran <sivayogeith@gmail.com>
2025-10-02 08:20:10 +0530

    feat: added countdown, date and time in your timezone and styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,39 @@
 <script lang="ts">
-	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
-	
-	let { children } = $props();
+  import "../app.css";
+  import favicon from "$lib/assets/favicon.png";
+
+  let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+  <link rel="icon" href={favicon} />
+  <style>
+    @import url("https://fonts.googleapis.com/css2?family=Saira+Stencil+One&family=Saira:ital,wght@0,100..900;1,100..900&display=swap");
+	
+	body {
+      font-family: "Saira", sans-serif;
+      font-optical-sizing: auto;
+      font-style: normal;
+      font-variation-settings: "wdth" 100;
+	  background: url("bg.svg");
+	  background-position: right;
+    }
+
+    .saira-stencil {
+      font-family: "Saira Stencil One", sans-serif;
+      font-weight: 400;
+      font-style: normal;
+    }
+
+	@font-face {
+		font-family: "DS-Digital";
+		src: url("DS-DIGIB.ttf");
+	}
+
+    .clock-font {
+      font-family: "DS-Digital", monospace;
+    }
+  </style>
 </svelte:head>
 
 {@render children?.()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,112 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import { onMount } from "svelte";
+  import favicon from "$lib/assets/favicon.png";
+
+  interface TimeCalc extends Object {
+    days: number;
+    hours: number;
+    minutes: number;
+    seconds: number;
+    distance: number;
+  }
+
+  const DEFAULT_TIMECALC = {
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+    distance: 0,
+  };
+
+  const shippingEnd = new Date("2025-10-02T03:59:59.000Z");
+  const votingEnd = new Date("2025-10-07T03:59:59.000Z");
+  const shopEnd = new Date("2025-10-11T03:59:59.000Z");
+
+  let shippingTimeCalc: TimeCalc = $state(DEFAULT_TIMECALC);
+  let votingTimeCalc: TimeCalc = $state(DEFAULT_TIMECALC);
+  let shopTimeCalc: TimeCalc = $state(DEFAULT_TIMECALC);
+
+  const countdownInterval = setInterval(function () {
+    let now = new Date().getTime();
+
+    shippingTimeCalc = getTime(shippingEnd.getTime() - now);
+    votingTimeCalc = getTime(votingEnd.getTime() - now);
+    shopTimeCalc = getTime(shopEnd.getTime() - now);
+
+    if (shopEnd.getTime() - now < 0) {
+      clearInterval(countdownInterval);
+    }
+  }, 1000);
+
+  const getTime = (distance: number) => {
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    return { days, hours, minutes, seconds, distance } as TimeCalc;
+  };
+
+  onMount(() => {
+    let now = new Date().getTime();
+
+    shippingTimeCalc = getTime(shippingEnd.getTime() - now);
+    votingTimeCalc = getTime(votingEnd.getTime() - now);
+    shopTimeCalc = getTime(shopEnd.getTime() - now);
+  });
+</script>
+
+<h1 class="text-6xl saira-stencil text-center mt-10">
+  When does Summer of Making <span class="text-red-800">END</span>?
+</h1>
+<div class="flex items-center flex-col md:h-[82dvh] gap-10 p-12">
+  <img src={favicon} class="h-[15rem] w-auto" alt="AAAAAAAApheus by @ren">
+  <div class="w-[70vw] 2xl:ms-[28vw]">
+    <h2 class="text-4xl">
+      Shipping ends in <span class="text-2xl"
+        >(at {shippingEnd.toLocaleString()} in your timezone)</span
+      >
+    </h2>
+    <h2 class="text-5xl font-bold text-red-800">
+      {#if shippingTimeCalc.distance < 0}
+        SHIPPING HAS ENDED!!!
+      {:else}
+        {shippingTimeCalc.days} days {shippingTimeCalc.hours} hours {shippingTimeCalc.minutes}
+        minutes {shippingTimeCalc.seconds} seconds
+      {/if}
+    </h2>
+  </div>
+  <div class="w-[70vw] 2xl:ms-[28vw]">
+    <h2 class="text-4xl">
+      Voting ends in <span class="text-2xl"
+        >(at {votingEnd.toLocaleString()} in your timezone)</span
+      >
+    </h2>
+    <h2 class="text-5xl font-bold text-red-800">
+      {#if votingTimeCalc.distance < 0}
+        VOTING HAS ENDED!!!
+      {:else}
+        {votingTimeCalc.days} days {votingTimeCalc.hours} hours {votingTimeCalc.minutes}
+        minutes {votingTimeCalc.seconds} seconds
+      {/if}
+    </h2>
+  </div>
+  <div class="w-[70vw] 2xl:ms-[28vw]">
+    <h2 class="text-4xl">
+      Shop closes in <span class="text-2xl"
+        >(at {shopEnd.toLocaleString()} in your timezone)</span
+      >
+    </h2>
+    <h2 class="text-5xl font-bold text-red-800">
+      {#if shopTimeCalc.distance < 0}
+        SHOP IS CLOSED!!!
+      {:else}
+        {shopTimeCalc.days} days {shopTimeCalc.hours} hours {shopTimeCalc.minutes}
+        minutes {shopTimeCalc.seconds} seconds
+      {/if}
+    </h2>
+  </div>
+</div>
+<footer class="text-2xl text-center mb-5">by sage with :3</footer>
